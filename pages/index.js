85fb15ae076d5b46a9f8b7f116cec6830c9047b3
Jvazqu61 <jvazqu61@uic.edu>
2022-01-26 16:05:33 -0600

    added styles to trending coins

--- pages/index.js
+++ pages/index.js
@@ -5,25 +5,20 @@ import Trending from "./components/Trending";
 
 export default function Home(props) {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">
-        crypto app
-      </h1>
+    <div className="p-10">
+     
       
-
-      <main className="columns-2xs">
-        {/* left side bar - trending  */}
-        <div className="w-[20rem] border-x-4 border-red-500">
-          left sidebar
-          <Trending trendingCoins={props.trending} />
-        </div>
-
-        {/* right - cards */}
-        <div className="w-[40rem] border-x-4 border-purple-400 ml-20">
-          <AllCoins />
-        </div>
+      {/* left side bar - trending  */}
+      
+        <Trending trendingCoins={props.trending} />
         
-      </main>
+
+      {/* right - cards */}
+      {/* <div className="w-[40rem] border-x-4 border-purple-400 ml-20">
+        <AllCoins />
+      </div> */}
+        
+      
 
       
     </div>
@@ -36,19 +31,16 @@ export async function getStaticProps() {
   try {
     var resTrending = await axios.get('https://api.coingecko.com/api/v3/search/trending');
     var resCoins = await axios.get('https://api.coingecko.com/api/v3/coins/list?include_platform=false');
-    console.log(resCoins);
+    // console.log(resCoins);
   } catch (error) {
     console.log(error);
   }
-
-
-
-
 
   // return a trending array and a regular array of coins
   return {
     props: {
       trending: resTrending.data.coins,
+      // AllCoins: resCoins.data,
     },
   }
 }
